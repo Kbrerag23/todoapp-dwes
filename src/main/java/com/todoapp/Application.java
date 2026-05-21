c6c51026351b6035686d53679d4d1730a84896c7
Adrián García Cabrera <adriangc43@educastur.es>
2026-05-22 00:14:39 +0200

    Añadida configuración de seguridad global para mostrar botón Authorize en Swagger

--- src/main/java/com/todoapp/Application.java
+++ src/main/java/com/todoapp/Application.java
@@ -1,9 +1,17 @@
 package com.todoapp;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(
+		name = "basicAuth",
+		type = SecuritySchemeType.HTTP,
+		scheme = "basic",
+		description = "Introduce tu email y contraseña del archivo import.sql para autenticarte"
+)
 public class Application {
 
 	public static void main(String[] args) {
